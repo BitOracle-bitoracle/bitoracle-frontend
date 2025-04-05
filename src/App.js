134e1868b45e0175744f40fade22ac044c8be3ff
YSH <yang5864@naver.com>
2025-04-05 19:52:13 +0900

    axios 제거

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import MainPage from "./Mainpage/MainPage";
 import "./App.css";
-import axios from "axios";
 
 function App() {
   useEffect(() => {
