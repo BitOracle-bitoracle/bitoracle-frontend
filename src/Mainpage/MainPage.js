e69b2f86c4cea1a60abedd13e55c32c7e2696dde
YSH <yang5864@naver.com>
2025-04-16 23:16:38 +0900

    메인페이지 패널 간격 변경

--- src/Mainpage/MainPage.js
+++ src/Mainpage/MainPage.js
@@ -1,19 +1,31 @@
 import React from "react";
+import IndicatorPanel from "./IndicatorPanel";
 import NewsSlider from "./NewsSlider";
-// import IndicatorPanel from "../components/IndicatorPanel"; // 👈 임시로 주석 처리
-
 import "./MainPage.css";
 
 const MainPage = () => {
   return (
-    <div className="main-container">
-      {/* <div className="indicator-panel">
-        <IndicatorPanel />
-      </div> */}
-      <div className="news-panel">
-        <NewsSlider />
+    <>
+      <div className="main-scroll-wrapper">
+        <div className="main-content">
+          <div className="main-container">
+            <div className="panel">
+              <IndicatorPanel />
+            </div>
+            <div className="panel">
+              <NewsSlider />
+            </div>
+          </div>
+          <div className="section-divider" />
+          <div className="bottom-panel">
+            <div className="prediction-chart-placeholder">
+              <h2>📈 비트코인 가격 예측 차트</h2>
+              <p>예측 차트가 이 영역에 표시됩니다.</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
