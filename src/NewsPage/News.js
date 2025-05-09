4d8bc8106e996f7f3e26e1d12b1b935e5467b1e8
6unYoung <mukmulpasta@naver.com>
2025-05-09 13:11:26 +0900

    temp file

---
+++ src/NewsPage/News.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./News.css";
+
+const News = () => {
+    console.log("News loaded");
+
+    return (
+        <div className="news-container">
+                <h1>News</h1>
+                <p>welcome here!</p>
+                <p>welcome here!</p>
+                <p>welcome here!</p>
+                <p>welcome here!</p>
+                <p>welcome here!</p>
+                <p>welcome here!</p>
+                <p>welcome here!</p>
+                <p>welcome here!</p>
+                <p>welcome here!</p>
+                <p>welcome here!</p>
+        </div>
+    );
+};
+
+export default News;
