9009a91265f74aac704ef21d5653b660e9b96d42
YSH <yang5864@naver.com>
2025-05-29 00:24:32 +0900

    포트폴리오 테이블과 연동되도록

--- src/PortfolioPage/PortfolioSummary.js
+++ src/PortfolioPage/PortfolioSummary.js
@@ -1,28 +1,33 @@
 import React from "react";
 import "./PortfolioSummary.css";
 
-const PortfolioSummary = ({ summary, totalRate }) => {
+const PortfolioSummary = ({ summary }) => {
+  const { totalBuy, totalNow, totalProfit, totalRate } = summary;
   return (
     <div className="overview-left">
       <div className="portfolio-summary">
         <div>
-          총 보유자산: <span className="value">￦193,601,543</span>
+          총 보유자산: <span className="value">{totalNow.toLocaleString()}￦</span>
         </div>
         <div>
-          총 매수: <span className="value">￦89,544,663</span>
+          총 매수: <span className="value">{totalBuy.toLocaleString()}￦</span>
         </div>
         <div>
-          총 평가: <span className="value">￦193,601,542</span>
+          총 평가: <span className="value">{totalNow.toLocaleString()}￦</span>
         </div>
         <div>
-          총 평가손익: <span className="red value">+￦104,056,880</span>
+          총 평가손익: <span className={`value ${totalProfit >= 0 ? 'red' : 'blue'}`}>
+            {totalProfit >= 0 ? '+' : '-'}{Math.abs(totalProfit).toLocaleString()}￦
+          </span>
         </div>
         <div>
-          총 평가수익률: <span className="red value">+116.21%</span>
+          총 평가수익률: <span className={`value ${totalProfit >= 0 ? 'red' : 'blue'}`}>
+            {totalProfit >= 0 ? '+' : '-'}{Math.abs(totalRate).toFixed(2)}%
+          </span>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default PortfolioSummary;
