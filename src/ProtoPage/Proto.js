920bdacacddceef45e27a9910d8cb0c7586d1c11
6unYoung <mukmulpasta@naver.com>
2025-06-15 20:56:23 +0900

    proto ui edit v3.4

--- src/ProtoPage/Proto.js
+++ src/ProtoPage/Proto.js
@@ -7,7 +7,33 @@ import "./Proto.css";
 const BASE_URL = "https://api.bitoracle.shop/api/predict";
 
 const Proto = () => {
-    const handlePrdictBtnClick = async (predictType) => {
+    const [isPredicted, setIsPredicted] = useState(true);
+
+    useEffect(() => {
+        const token = localStorage.getItem("access");
+
+        if (!token) {
+            alert("로그인이 필요합니다.");
+            return;
+        }
+
+        axios
+            .get(`${BASE_URL}/check`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                withCredentials: true, // Cookie 전달함.
+            })
+            .then((res) => {
+                console.log("user isPredicted: ", res);
+                setIsPredicted(res.data.data.predicted);
+            })
+            .catch((error) => {
+                console.error("Fail to check isPredicted.", error);
+            });
+    }, []);
+
+    const handlePredictBtnClick = async (predictType) => {
         const token = localStorage.getItem("access");
 
         if (!token) {
@@ -22,8 +48,8 @@ const Proto = () => {
                 },
                 withCredentials: true, // Cookie 전달함.
             });
-            alert(`${predictType.upDown}을 선택하셨습니다.`);
             console.log("Success to post predict.", res);
+            alert(`${predictType.upDown}을 선택하셨습니다.`);
         } catch (error) {
             console.error("Fail to post predict.", error);
         }
@@ -37,17 +63,15 @@ const Proto = () => {
                 <div className="btn-wrapper">
                     <button
                         className="prediction-btn"
-                        onClick={() => {
-                            handlePrdictBtnClick({ upDown: "UP" });
-                        }}
+                        // onClick={() => handlePredictBtnClick({ upDown: "UP" })}
+                        disabled={isPredicted}
                     >
                         업
                     </button>
                     <button
                         className="prediction-btn"
-                        onClick={() => {
-                            handlePrdictBtnClick({ upDown: "DOWN" });
-                        }}
+                        // onClick={() => {handlePredictBtnClick({ upDown: "DOWN" });}}
+                        disabled={isPredicted}
                     >
                         다운
                     </button>
@@ -73,7 +97,7 @@ const CoinIndex = () => {
         axios
             .get(`${BASE_URL}/midnight`)
             .then((res) => {
-                console.log("today 0d coin data:", res);
+                console.log("today 0d coin data:", res.data);
                 setPrice(res.data.data.price);
             })
             .catch((error) => {
@@ -83,8 +107,11 @@ const CoinIndex = () => {
 
     return (
         <div className="inform-wrapper">
-            <h4>내일 0시 가격은 오늘 0시 가격보다 높을까요?</h4>
-            <span className="inform-box">오늘 0시 비트코인: {price}</span>
+            <h1>가격예측</h1>
+            <h4>내일 00시 비트코인은 오늘보다 오를까요?</h4>
+            <div className="middle-placeholder"/>
+            <h4 className="inform-box">오늘 00시 비트코인</h4>
+            <p className="inform-value">{price}원</p>
             {/* <span className="inform-box">비트코인 현재가: {}</span> */}
         </div>
     );
@@ -107,7 +134,7 @@ const StatCalendar = () => {
         return null;
     };
 
-    const getPredictions = async () => {
+    useEffect(() => {
         const token = localStorage.getItem("access");
 
         if (!token) {
@@ -115,32 +142,30 @@ const StatCalendar = () => {
             return;
         }
 
-        try {
-            const res = await axios.get(`${BASE_URL}/calendar`, {
+        axios
+            .get(`${BASE_URL}/calendar`, {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
                 withCredentials: true, // Cookie 전달함.
-            });
-            console.log("Success to get predictions.", res);
+            })
+            .then((res) => {
+                console.log("Success to get calendar.", res.data);
 
-            const resultMap = {};
-            res.data.data?.forEach((item) => {
-                resultMap[item.created_at] = item.correct;
+                const resultMap = {};
+                res.data.data?.forEach((item) => {
+                    resultMap[item.created_at] = item.correct;
+                });
+                setPredictions(resultMap);
+            })
+            .catch((error) => {
+                console.error("Fail to get predictions.", error);
             });
-            setPredictions(resultMap);
-        } catch (error) {
-            console.error("Fail to get predictions.", error);
-        }
-    };
-
-    useEffect(() => {
-        getPredictions();
     }, []);
 
     return (
         <Calendar
-            tileContent={tileContent}
+            // tileContent={tileContent}
             maxDate={new Date()} // 오늘까지만 선택 가능
             formatDay={(locale, date) => date.getDate()}
         />
@@ -166,7 +191,7 @@ const StatText = () => {
                 withCredentials: true, // Cookie 전달함.
             })
             .then((res) => {
-                console.log("Success to get stats:", res);
+                console.log("Success to get stats:", res.data);
                 setStats(res.data.data);
             })
             .catch((error) => {
@@ -175,21 +200,25 @@ const StatText = () => {
     }, []);
 
     return (
-        <div className="proto-stat">
-            {" "}
+        <div>
             {stats ? (
-                <div>
-                    <span className="stats-text">총 {stats.trial}</span>
-                    <span className="stats-text">성공 {stats.success}</span>
-                    <span className="stats-text">실패 {stats.failure}</span>
-                    <br />
-                    <span className="stats-text">
-                        승률{" "}
-                        {stats.trial > 0
-                            ? (stats.success * 100) / stats.trial
-                            : 0}
-                        %
-                    </span>
+                <div className="proto-stat">
+                    <div className="stat-row">
+                        <span className="stats-text">총 {stats.trial}</span>
+                        <span className="stats-text">성공 {stats.success}</span>
+                        <span className="stats-text">실패 {stats.failure}</span>
+                    </div>
+                    <div className="stat-row">
+                        <span className="stats-text">
+                            승률{" "}
+                            {stats.trial > 0
+                                ? Math.round(
+                                      (stats.success * 100) / stats.trial
+                                  )
+                                : 0}
+                            %
+                        </span>
+                    </div>
                 </div>
             ) : (
                 <span className="stats-text">아직 통계가 없어요!</span>
